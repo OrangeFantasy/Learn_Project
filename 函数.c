a477a65c73bf7cf2890b17df37f8ad79e35d8b60
OrangeFantasy <80752313+OrangeFantasy@users.noreply.github.com>
2021-03-17 00:45:28 +0800

    Add files via upload

---
+++ 函数.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+	return 0;
+}
+
+
+
+////求最小值
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int max = get_max(a, b);
+//	printf("max=%d\n", max);
+//
+//	return 0;
+//}
+
+
+
+//int main()  //memset 函数
+//{
+//	char arr[] = "hello world";
+//	memset(arr, '*', 5);
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
+
+
+
+//int main()  //strcpy 函数 cplusplus.com
+//{
+//	char arr1[] = "bit" ;
+//	char arr2[20] = "******" ;
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+
+
+////加法计算
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int sum=Add(a, b);
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
